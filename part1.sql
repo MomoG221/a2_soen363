b8c1a5b0f4720bf6d4e85bdfd4bbf1e51338dd76
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-11-04 14:44:10 -0500

    first table created

--- part1.sql
+++ part1.sql
@@ -1,4 +1,27 @@
 create table movies (
     movies_id SERIAL PRIMARY KEY,
-
+    watchmode_id INT,
+    title VARCHAR(255),
+    original_title VARCHAR(255),
+    plot_overview TEXT,
+    type VARCHAR(255),
+    runtime_minutes INT,
+    year INT,
+    end_year INT,
+    release_date DATE,
+    imdb_id VARCHAR(255),
+    tmdb_id INT,
+    tmdb_type VARCHAR(255),
 );
+
+create table movies_genre();
+
+create table genre();
+
+create table ratings();
+
+create country movies_country();
+
+create country country();
+
+
